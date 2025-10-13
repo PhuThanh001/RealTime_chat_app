74668f8bc8a5b20efa1c1fc1653e4bff6dd0bf3f
PhuThanh001 <phunguyensc2001@gmail.com>
2025-10-13 19:25:01 +0700

    Refactor auth and message routers to remove unused endpoints and implement middleware for improved security

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -7,10 +7,13 @@ import messageRouters from "./routers/message.router.js"
 dotenv.config();
 const app  = express();
 
-
 const PORT = process.env.PORT || 3000 ;
 
+
+app.use(express.json({limit :"5mb"})); //req.body
+app.use(cors({origin: ENV.ClIENT_URL , credentials:true }))
 app.use('/api/auth' , authRouters)
 app.use('/api/message' , messageRouters)
+app.use(cookieParers());
 
 app.listen(PORT , () => console.log('server running on port 3000 ' + PORT));
